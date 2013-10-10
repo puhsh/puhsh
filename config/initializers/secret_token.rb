72eadcb390b8de92bff7faf70cf96c447dc50204
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2013-10-10 13:14:04 -0500

    Init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Puhsh::Application.config.secret_token = '4ee545518c182962db256546fce7c526d18d43eea7c5d8473c2add119702a8741832d619a437c6fb7ec29dcd13defbfeacc0e72245d19396313c677aa258b858'
