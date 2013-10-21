4366cbfebd382ecce013a8f41eeb17bcb2ae820a
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2013-10-21 11:44:08 -0500

    Specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,22 @@
 require 'spec_helper'
 
 describe User do
+
+  it { should have_many(:posts) }
+
   context '.default_role' do
     let(:user) { FactoryGirl.build(:user) }
 
     it 'sets the user role to member' do
       user.save
       expect(user.reload.has_role?(:member)).to be_true
+    end
+
+    it 'does not change the role on update' do
+      user.add_role(:admin)
+      user.save
+      user.reload.touch
+      expect(user.reload.has_role?(:admin)).to be_true
     end
 
     it 'never sets the user role to admin' do
@@ -17,5 +27,4 @@ describe User do
 
   describe '.find_for_facebook_oauth' do
   end
-
 end
