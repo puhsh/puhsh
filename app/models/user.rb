3560ad3f36d87183b150a4e14f66e5904c01e78f
Arthur Dexter <adexter@dexman.net>
2013-11-10 16:49:00 -0600

    Touch :expires_at when renewing the token.

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,7 +52,7 @@ class User < ActiveRecord::Base
   end
 
   def generate_access_token!
-    self.access_token.nil? ? AccessToken.create!(user: self) : self.access_token.touch
+    self.access_token.nil? ? AccessToken.create!(user: self) : self.access_token.touch(:expires_at)
   end
 
   protected
