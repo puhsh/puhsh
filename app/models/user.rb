22d7d9f00ec055883fabeedd12270f588acdc77d
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2013-11-11 11:29:16 -0600

    Possibility of granting 20 points when an alpha user signs up.

--- app/models/user.rb
+++ app/models/user.rb
@@ -80,6 +80,8 @@ class User < ActiveRecord::Base
 
   def reward_stars
     Star.create(user: self, amount: 10, reason: :alpha_registration)
+    # TODO Determine if we want to give 20 points at the start
+    # Star.create(user: self, amount: 10, reason: :new_account)
   end
 end
 
