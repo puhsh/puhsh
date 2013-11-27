d76af4e3581556c1cada9cd2f19e2f212305fc7b
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2013-11-27 15:27:11 -0600

    Ability refactoring.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,14 +7,14 @@ class Ability
       can :manage, :all
     else
       can :manage, User, id: user.id
-      can :manage, Device
+      can :manage, Device, user_id: user.id
       can :read, Star, user_id: user.id
       can :manage, Invite, user_id: user.id
       can :read, Category
       can :read, Subcategory
       can :manage, Post, user_id: user.id
       can :manage, PostImage
-      can :manage, FollowedCity
+      can :manage, FollowedCity, user_id: user.id
       can :read, City
     end
   end
