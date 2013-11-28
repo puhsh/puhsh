32b693875032d72c74250249908a409e6b5c79ff
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2013-11-28 15:16:03 -0600

    Not using SoftLayer object storage.

--- config/initializers/softlayer.rb
+++
@@ -1,7 +0,0 @@
-require "softlayer/object_storage"
-
-@softlayer = SoftLayer::ObjectStorage::Connection.new({
-  username: 'SLOS290320-2:puhsh',
-  api_key: 'dac2f876ceda6296cee89ddafacb0e115b3e96cb946456a1580ab2760502d29b',
-  datacenter: :dal05
-})
