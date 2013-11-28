e6cc8a6de756c8381b2d88612942baa24dc47814
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2013-11-27 20:01:43 -0600

    Whoops

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -3,5 +3,5 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr_cassettes'
   c.hook_into :webmock
-  config.ignore_hosts 'codeclimate.com'
+  c.ignore_hosts 'codeclimate.com'
 end
