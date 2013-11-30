31aae971ba043da16d93796ecc909425d88767d1
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2013-11-30 16:06:31 -0600

    Terrible merge...wtf happened.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,9 +2,5 @@ require 'spec_helper'
 
 describe Item do
   it { should belong_to(:post) }
-<<<<<<< HEAD:spec/models/item_spec.rb
   it { should have_many(:offers) }
-=======
-  it { should have_attached_file(:image) }
->>>>>>> Specs:spec/models/post_image_spec.rb
 end
--- spec/models/post_image_spec.rb
+++ spec/models/post_image_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe PostImage do
   it { should belong_to(:post) }
+  it { should have_attached_file(:image) }
 end
