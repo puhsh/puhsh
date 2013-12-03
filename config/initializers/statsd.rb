b154e95303efa4e7d88ff68592c0cc73ad84f133
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2013-12-02 22:17:47 -0600

    Try shopify's gem...

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -1,1 +1,5 @@
-$statsd = Statsd.new '75.126.213.99', 8125
+StatsD.server = '75.126.213.99:8125'
+StatsD.logger = Rails.logger
+StatsD.mode = :production
+StatsD.prefix = 'puhsh'
+StatsD.default_sample_rate = 0.1
