e993115648810847062298f0202cc9853a340683
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2013-12-02 21:52:42 -0600

    Tweaks

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -1,3 +1,15 @@
+def send_event_to_statsd(name, payload)
+  action = payload[:action] || :increment
+  measurement = payload[:measurement]
+  value = payload[:value]
+  key_name = "#{name.to_s.capitalize}.#{measurement}"
+  $statsd.__send__ action.to_s, key_name, (value || 1)
+end
+
+ActiveSupport::Notifications.subscribe /performance/ do |name, start, finish, id, payload| 
+  send_event_to_statsd(name, payload) if Rails.env.production?
+end
+
 ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |*args| 
   event = ActiveSupport::Notifications::Event.new(*args)
   controller = event.payload[:controller]
@@ -10,16 +22,4 @@ ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |*a
   ActiveSupport::Notifications.instrument :performance, :action => :timing, :measurement => "#{key}.db_time", :value => event.payload[:db_runtime]
   ActiveSupport::Notifications.instrument :performance, :action => :timing, :measurement => "#{key}.view_time", :value => event.payload[:view_runtime]
   ActiveSupport::Notifications.instrument :performance, :measurement => "#{key}.status.#{status}" 
-end
-
-def send_event_to_statsd(name, payload)
-  action = payload[:action] || :increment
-  measurement = payload[:measurement]
-  value = payload[:value]
-  key_name = "#{name.to_s.capitalize}.#{measurement}"
-  $statsd.__send__ action.to_s, key_name, (value || 1)
-end
-
-ActiveSupport::Notifications.subscribe /performance/ do |name, start, finish, id, payload| 
-  send_event_to_statsd(name, payload) if Rails.env.production?
 end
