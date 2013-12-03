8bc81966dd223e3bc99baf654c0b27f8197f0ca1
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2013-12-03 13:35:43 -0600

    New host

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -1,5 +1,5 @@
-StatsD.server = '75.126.213.99:8125'
+StatsD.server = '10.81.99.131:8125'
 StatsD.logger = Rails.logger
 StatsD.mode = :production
 StatsD.prefix = 'puhsh'
-StatsD.default_sample_rate = 0.1
+StatsD.default_sample_rate = 0.8
