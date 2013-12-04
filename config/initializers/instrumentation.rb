ec5ff035859e693dd95f19ed39dc1ebefdad71bf
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2013-12-04 07:23:54 -0600

    Integrating ActiveSupport::Notifications with Statsd

---
+++ config/initializers/instrumentation.rb
@@ -0,0 +1,28 @@
+ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |*args|
+  event = ActiveSupport::Notifications::Event.new(*args)
+  controller = event.payload[:controller]
+  action = event.payload[:action]
+  format = event.payload[:format] || "all"
+  format = "all" if format == "*/*"
+  status = event.payload[:status]
+  key = "#{controller}.#{action}.#{format}"
+
+  ActiveSupport::Notifications.instrument :performance, 
+                                          :action => :timing, 
+                                          :measurement => "#{key}.total_duration", 
+                                          :value => event.duration
+  ActiveSupport::Notifications.instrument :performance, 
+                                          :action => :timing, 
+                                          :measurement => "#{key}.db_time", 
+                                          :value => event.payload[:db_runtime]
+  ActiveSupport::Notifications.instrument :performance, 
+                                          :action => :timing, 
+                                          :measurement => "#{key}.view_time", 
+                                          :value => event.payload[:view_runtime]
+  ActiveSupport::Notifications.instrument :performance, 
+                                          :measurement => "#{key}.status.#{status}"
+end
+
+ActiveSupport::Notifications.subscribe /performance/ do |name, start, finish, id, payload|
+  Puhsh::Statsd.send_event_to_statsd(name, payload)
+end
---
+++ lib/puhsh/statsd.rb
@@ -0,0 +1,13 @@
+module Puhsh
+  module Statsd
+    def self.send_event_to_statsd(name, payload)
+      action = payload[:action] || :increment
+      measurement = payload[:measurement]
+      value = payload[:value]
+      key_name = "#{name.to_s.capitalize}.#{measurement}"
+      batch = Statsd::Batch.new($statsd)
+      batch.__send__ action.to_s, "production.#{key_name}", (value || 1)
+      batch.flush
+    end
+  end
+end
