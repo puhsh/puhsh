34005d00e3a40e7c1014bacf2fdf39ed6048656a
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2013-12-06 13:29:49 -0600

    [#69] DRY'd this junk up

--- app/models/app_invite.rb
+++ app/models/app_invite.rb
@@ -1,5 +1,6 @@
 class AppInvite < ActiveRecord::Base
-  OFFSET = 2215
+  include Puhsh::AlphaQueue
+
   attr_accessible :status, :user
   acts_as_list
   symbolize :status, in: [:inactive, :active], scopes: true, methods: true, validate: false
@@ -23,24 +24,5 @@ class AppInvite < ActiveRecord::Base
 
   def self.total_waiting
     AppInvite.last.position - self.total_active
-  end
-
-  def activate!
-    self.update_attributes(status: :active)
-    self.user.devices.each { |x| x.fire_notification!("Your invite to Puhsh has been accepted.", :app_invite_accepted) } if self.user
-  end
-
-  def users_in_front_of_user
-    (self.position - self.class.total_active) - 1
-  end
-
-  def current_position
-    self.position - self.class.total_active
-  end
-
-  protected
-
-  def set_status
-    self.status = :inactive
   end
 end
---
+++ lib/puhsh/alpha_queue.rb
@@ -0,0 +1,26 @@
+module Puhsh
+  module AlphaQueue
+
+    OFFSET = 2215
+
+    def activate!
+      self.update_attributes(status: :active)
+      self.user.devices.each { |x| x.fire_notification!("Your invite to Puhsh has been accepted.", :app_invite_accepted) } if self.user
+    end
+
+    def users_in_front_of_user
+      (self.position - self.class.total_active) - 1
+    end
+
+    def current_position
+      self.position - self.class.total_active
+    end
+
+    protected
+
+    def set_status
+      self.status = :inactive
+    end
+
+  end
+end
