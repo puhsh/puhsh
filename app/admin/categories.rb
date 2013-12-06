6cd183e37aeeee785f68380c94dc1333d23e8a5a
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2013-12-06 14:10:21 -0600

    Category form in Admin

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register Category do
-  
+  form do |f|
+    f.inputs '' do
+      f.input :name
+      f.input :status, as: :select, collection: options_for_select([:inactive, :active], :inactive), include_blank: false
+    end
+    f.actions
+  end
 end
