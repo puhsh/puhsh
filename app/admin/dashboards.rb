bdc88b36732b175c6288c5d3fa978ba687727a89
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-01-04 11:23:22 -0600

    Deprecation warning = gone

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -1,40 +1,25 @@
-ActiveAdmin::Dashboards.build do  
-  section "Recent Users (15)" do
-    table_for User.order('created_at desc').limit(15) do
-      column :id do |user|
-        link_to user.id, admin_user_path(user)
+ActiveAdmin.register_page 'Dashboard' do
+  content title: 'Overview' do
+    div do
+      span do
+        "Recent Users (15)"
       end
-      column :uid
-      column :name
-      column :first_name
-      column :last_name
-      column :zipcode
-      column :facebook_email
-      column :contact_email
-      column :created_at
-      column :current_sign_in_at
-      column :last_sign_in_at
-      column :star_count
-    end
-  end
-
-  section "Recent Invites (15)" do
-    table_for Invite.order('created_at desc').limit(15) do
-      column :user_id do |invite|
-        link_to invite.user_id, admin_user_path(invite.user)
+      table_for User.order('created_at desc').limit(15) do
+        column :id do |user|
+          link_to user.id, admin_user_path(user)
+        end
+        column :uid
+        column :name
+        column :first_name
+        column :last_name
+        column :zipcode
+        column :facebook_email
+        column :contact_email
+        column :created_at
+        column :current_sign_in_at
+        column :last_sign_in_at
+        column :star_count
       end
-      column :uid_invited
-      column :created_at
-    end
-  end
-
-  section "Recent Stars (15)" do
-    table_for Star.order('created_at desc').limit(15) do
-      column :user_id do |invite|
-        link_to invite.user_id, admin_user_path(invite.user)
-      end
-      column :amount
-      column :event
     end
   end
 end
