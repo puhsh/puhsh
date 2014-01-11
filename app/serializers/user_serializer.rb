e14948285aa58d68f2b434b5a716e1d5433e9d71
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-01-10 20:26:09 -0600

    Show if the current user is following the user when serializing the user.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,13 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :uid, :name, :first_name, :last_name, :avatar_url, :zipcode, :location_description, :latitude,
              :longitude, :home_city, :gender, :facebook_email, :contact_email, :posts_count, :posts_flagged_count,
-             :app_invite, :star_count
+             :app_invite, :star_count, :is_following
 
   has_one :home_city
   has_one :app_invite
   has_one :android_app_invite
+
+  def is_following
+    current_user.following?(object)
+  end
 end
