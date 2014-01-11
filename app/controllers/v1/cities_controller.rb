8e4cbf3d8a7cc529b1dfe71fa47f36937a6cea1c
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-01-11 15:21:07 -0600

    Need some pagination

--- app/controllers/v1/cities_controller.rb
+++ app/controllers/v1/cities_controller.rb
@@ -1,10 +1,10 @@
 class V1::CitiesController < V1::ApiController
   before_filter :authenticate_user!
   before_filter :verify_access_token
-  load_and_authorize_resource
+  # load_and_authorize_resource
 
   def index
     @cities = City.where(id: current_user.followed_city_ids.members)
-    render json: @cities
+    render_paginated @cities
   end
 end
