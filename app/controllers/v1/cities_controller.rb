4c49054beb937e2084fd2b74a5410e5066cf0caa
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-01-11 15:24:07 -0600

    CanCan

--- app/controllers/v1/cities_controller.rb
+++ app/controllers/v1/cities_controller.rb
@@ -1,7 +1,7 @@
 class V1::CitiesController < V1::ApiController
   before_filter :authenticate_user!
   before_filter :verify_access_token
-  # load_and_authorize_resource
+  load_and_authorize_resource
 
   def index
     @cities = City.where(id: current_user.followed_city_ids.members)
