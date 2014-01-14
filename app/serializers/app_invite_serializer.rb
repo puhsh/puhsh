6d10b46f1c5a90f688cf2fd81a5bbd8c08bed1ff
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-01-14 12:58:27 -0600

    No more device type

--- app/serializers/app_invite_serializer.rb
+++ app/serializers/app_invite_serializer.rb
@@ -1,5 +1,5 @@
 class AppInviteSerializer < ActiveModel::Serializer
-  attributes :id, :status, :total_users_waiting, :current_position, :device_type
+  attributes :id, :status, :total_users_waiting, :current_position
 
   def total_users_waiting
     object.class.total_waiting
