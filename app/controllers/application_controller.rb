182e7c1a0c3d0f825502298d0f18b8982973142e
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-01-17 14:50:06 -0600

    How do i type?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :check_if_prodution
+  before_filter :check_if_production
 
   def peek_enabled?
     current_user_admin?
