46b9f34653cfa03fc41ada97e56371a2c1042f6d
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-01-25 16:30:53 -0600

    We don't need sunspot rails

--- config/deploy.rb
+++ config/deploy.rb
@@ -124,12 +124,12 @@ namespace :deploy do
   namespace :solr do
     desc 'start solr'
     task :start, :roles => :app, :except => { :no_release => true } do
-      run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec sunspot-solr start --port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/pids"
+      run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake sunspot:solr:start"
     end
 
     desc 'stop solr'
     task :stop, :roles => :app, :except => { :no_release => true } do
-      run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec sunspot-solr stop --port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/pids"
+      run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake sunspot:solr:stop" 
     end
   end
 end
