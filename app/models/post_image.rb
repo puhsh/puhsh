ab4ea1bbce912504a577009e1cffefb85e1f88ba
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-01-25 17:32:07 -0600

    Store original size

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,7 +3,7 @@ class PostImage < ActiveRecord::Base
   
   # Paperclip Attributes
   has_attached_file :image,
-                    styles: {},
+                    styles: { original: ['1280x1280#' , :png] },
                     s3_permissions: :public_read,
                     path: "posts/:post_id/post_images/:id.png"
 
