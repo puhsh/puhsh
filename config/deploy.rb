c897d7fc26fd80ece9410d9ee912e0212d450890
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-01-26 08:59:16 -0600

    Sunspot

--- config/deploy.rb
+++ config/deploy.rb
@@ -124,12 +124,12 @@ namespace :deploy do
   namespace :solr do
     desc 'start solr'
     task :start, :roles => :app, :except => { :no_release => true } do
-      run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake sunspot:solr:start"
+      run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec bundle exec sunspot-solr start --port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=tmp"
     end
 
     desc 'stop solr'
     task :stop, :roles => :app, :except => { :no_release => true } do
-      run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake sunspot:solr:stop" 
+      run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec bundle exec sunspot-solr stop --port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=tmp"
     end
   end
 end
