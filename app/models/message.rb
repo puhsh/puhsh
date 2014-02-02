a6f3599d794699fa0fbae079eabc6cd06a5bf68d
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-02 15:09:02 -0600

    Scopes for easy querying of messages.

--- app/models/message.rb
+++ app/models/message.rb
@@ -11,4 +11,9 @@ class Message < ActiveRecord::Base
   validates :content, presence: true, length: { maximum: 160 }
 
   # Scopes
+  scope :by_sender, ->(sender) { where(sender_id: sender.id) }
+  scope :by_recipient, ->(recipient) { where(recipient_id: recipient.id) }
+  scope :grouped_by_recipient, group(:recipient_id)
+  scope :between_sender_and_recipient, ->(sender, recipient) { where('(sender_id = ? and recipient_id = ?) or (sender_id = ? and recipient_id = ?)', sender, recipient, recipient, sender) } 
+  scope :recent, order('created_at desc')
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -31,4 +31,16 @@ describe Message do
       expect(message.reload).to_not be_read
     end
   end
+
+  describe '.between_sender_and_recipient' do
+    let!(:sender) { FactoryGirl.create(:user) }
+    let!(:recipient) { FactoryGirl.create(:user) }
+    let!(:message) { FactoryGirl.create(:message, sender: sender, recipient: recipient, content: 'Test message' ) }
+    let!(:message2) { FactoryGirl.create(:message, sender: recipient, recipient: sender, content: 'Test message' ) }
+
+    it 'returns all messages between two individuals' do
+      expect(Message.between_sender_and_recipient(sender, recipient)).to include(message)
+      expect(Message.between_sender_and_recipient(sender, recipient)).to include(message2)
+    end
+  end
 end
