f887f52425f521a44e06696d69290f4d89880133
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-08 10:16:31 -0600

    Ripping out bootstrap in favor of foundation.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,7 @@
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
+//= require foundation
 //= require peek.js
 //= require peek/views/performance_bar
+
+$(function(){ $(document).foundation(); });
