a68623e79b1abc8aac15fe91968b69aa4de83d03
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-07 19:20:22 -0600

    Wired up other mandrill emails to send when the template is finished.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,10 +4,10 @@ class UserMailer < MandrillMailer::TemplateMailer
   def welcome_email(user)
     @user = user
     mandrill_mail template: 'Welcome',
-                    to: user.contact_email,
+                    to: @user.contact_email,
                     vars: { 
-                      'USER_FIRST_NAME' => user.first_name,
-                      'USER_EMAIL' => user.contact_email,
+                      'USER_FIRST_NAME' => @user.first_name,
+                      'USER_EMAIL' => @user.contact_email,
                       'CURRENT_YEAR' => Date.today.year
                     },
                     inline_css: true
@@ -15,6 +15,51 @@ class UserMailer < MandrillMailer::TemplateMailer
 
   def new_post_email(post)
     @post = post
-    @user = post.user
+    @user = @post.user
+    @post_url = ''
+    mandrill_mail template: 'item-posted',
+                  to: @user.contact_email,
+                  vars: {
+                    'USER_FIRST_NAME' => @user.first_name,
+                    'POST_TITLE' => @post.title,
+                    'POST_URL' => @post_url,
+                    'CURRENT_YEAR' => Date.today.year
+                  },
+                  inline_css: true
+  end
+
+  def new_message_email(message)
+    @message = message
+    @sender = @message.sender
+    @recipient = @message.recipient
+    @content = @message.content
+    mandrill_mail template: 'message-received',
+                  vars: {
+                    'RECIPIENT_FIRST_NAME' => @recipient.first_name,
+                    'SENDER_FIRST_NAME' => @sender.first_name,
+                    'SENDER_LAST_NAME' => @sender.last_name,
+                    'SENDER_AVATAR_URL' => @sender.avatar_url,
+                    'MESSAGE_CONTENT' => @content,
+                    'CURRENT_YEAR' => Date.today.year
+                  },
+                  inline_css: true
+
+  end
+
+  def new_question_email(question)
+    @question = question
+    @post = @question.item.post
+    @question_user = @question.user
+    @post_user = @post.user
+    mandrill_mail template: 'comment-posted',
+                  vars: {
+                    'POST_USER_FIRST_NAME' => @post_user.first_name,
+                    'QUESTION_USER_FIRST_NAME' => @question_user.first_name,
+                    'QUESTION_USER_LAST_NAME' => @question_user.last_name,
+                    'QUESTION_USER_AVATAR_URL' => @question_user.avatar_url,
+                    'QUESTION_CONTENT' => @question.content,
+                    'CURRENT_YEAR' => Date.today.year
+                  },
+                  inline_css: true
   end
 end
