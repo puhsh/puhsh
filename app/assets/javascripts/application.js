cf89ddeb44d51e85e37f901d3da022fa1024c236
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-09 08:11:59 -0600

    The roller coaster of compiling assets.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,6 @@
 //= require peek.js
 //= require peek/views/performance_bar
 //= require moment/moment.js
-//= require angular/angular.js
+//= require angular/angular.min.js
 
 $(function(){ $(document).foundation(); });
