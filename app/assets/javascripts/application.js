ba549d735da52d9124bd0cb0b3249af0e872ebaf
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-09 09:07:58 -0600

    Precompile issues?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,8 @@
 //= require jquery
 //
 //
-//
 //= require foundation
 //= require peek.js
 //= require peek/views/performance_bar
-//= require moment/moment.js
-//= require angular/angular.min.js
-//= require sly/src/sly.js
-//= require spin.js/spin.js
 
-$(function() { 
-  $(document).foundation(); 
-  console.log('Foundation loaded');
-});
+$(function(){ $(document).foundation(); });
