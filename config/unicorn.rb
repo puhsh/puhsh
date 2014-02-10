d9d1ced164a51c6b95f0a7d59091d70af13de0bc
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-09 18:19:39 -0600

    Tracking down something..

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -32,11 +32,10 @@ before_fork do |server, worker|
   defined?(ActiveRecord::Base) and 
     ActiveRecord::Base.connection.disconnect!
 
-  # Rolling restarts
   old_pid = "#{rails_root}/tmp/pids/unicorn.puhsh.pid.oldbin"
   if File.exists?(old_pid) && server.pid != old_pid
     begin
-      server.logger.info("sending QUIT to #{old_pid}")
+      server.logger.info("worker #{worker.nr} sending QUIT to #{old_pid}")
       Process.kill("QUIT", File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
       # pid already gone
