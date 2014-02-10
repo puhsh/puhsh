eeea7cd8b25f6fd9ffe56fd4d177bcb1d41a03c8
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-10 07:12:57 -0600

    Spin.js and Sly.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,5 +5,7 @@
 //= require peek.js
 //= require peek/views/performance_bar
 //= require moment/moment.js
+//= require sly/src/sly.js
+//= require spin.js/spin.js
 
 $(function(){ $(document).foundation(); });
