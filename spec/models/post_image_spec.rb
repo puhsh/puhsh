28061e9c8270171e39eed4231e5d273e441c0b4e
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-11 12:52:33 -0600

    Specs

--- spec/models/post_image_spec.rb
+++ spec/models/post_image_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 describe PostImage do
   it { should belong_to(:post) }
   it { should have_attached_file(:image) }
+  it { should validate_attachment_content_type(:image).allowing('image/png', 'image/jpg', 'image/jpeg') }
 end
