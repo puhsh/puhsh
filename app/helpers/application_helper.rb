2ededd4c22731357ba61e32ea539dfc4e8b110d4
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-11 15:21:58 -0600

    Helper to get other avatar sizes from facebook

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def facebook_avatar_url(user, size = nil)
+    valid_sizes = [:large, :square, :normal]
+
+    if size && valid_sizes.include?(size)
+      user.avatar_url.split('?').first + "?type=#{size.to_s}"
+    else
+      user.avatar_url
+    end
+  end
 end
