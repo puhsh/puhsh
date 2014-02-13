66e723706311568fc19c0ad37017b5f4f33dc166
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-13 08:14:59 -0600

    Set a session variable that allows the Rails app to talk to its own API, bypassing authentication. Let there also be angular.

--- app/assets/javascripts/angular/controllers/posts_controller.js
+++ app/assets/javascripts/angular/controllers/posts_controller.js
@@ -1,2 +1,7 @@
+var puhsh = angular.module('puhshApp', []);
+
 puhsh.controller('PostsController', function($scope, $http) {
+  $http.get('/v1/posts').success(function(data, status, headers, config) {
+    $scope.items = data.items;
+  });
 });
