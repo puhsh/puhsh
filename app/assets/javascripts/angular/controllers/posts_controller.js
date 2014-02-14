0ad7a45ffaa1bb1457bdfe96f473f286acdd415d
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-14 14:11:08 -0600

    Started work on actually showing posts

--- app/assets/javascripts/angular/controllers/posts_controller.js
+++
@@ -1,7 +0,0 @@
-var puhsh = angular.module('puhshApp', []);
-
-puhsh.controller('PostsController', function($scope, $http) {
-  $http.get('/v1/posts').success(function(data, status, headers, config) {
-    $scope.items = data.items;
-  });
-});
