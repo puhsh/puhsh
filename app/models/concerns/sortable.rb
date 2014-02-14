e0130603630bec181f300c3831e3e76ab0923273
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-13 22:23:23 -0600

    Group messages by recipient, sorted by activity.

--- app/models/concerns/sortable.rb
+++ app/models/concerns/sortable.rb
@@ -4,5 +4,7 @@ module Sortable
   included do
     scope :recent, order('created_at desc')
     scope :oldest, order('created_at asc')
+    scope :latest, order('updated_at desc')
+    scope :newest, order('id desc')
   end
 end
