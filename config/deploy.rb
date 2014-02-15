c825bb57f734eea8eeeb4234decd509c1faf28ee
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-15 13:17:32 -0600

    Specify Gemfile location. Sometimes Unicorn gets confused I guess.

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :rvm_ruby_version, 'ruby-2.0.0-p247@puhsh'
 set :max_asset_age, 2 
 SSHKit.config.command_map[:whenever] = "bundle exec whenever"
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
-set :keep_releases, 100
+set :keep_releases, 5
 
 namespace :deploy do
 
