d9e65c2b9a118b8df1bcc9e71c95c28f09cc0534
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-16 08:20:15 -0600

    Post with images styling.

--- app/assets/javascripts/angular/controllers/posts_controller.js
+++ app/assets/javascripts/angular/controllers/posts_controller.js
@@ -3,6 +3,8 @@ var puhsh = angular.module('puhshApp', []);
 puhsh.controller('PostsController', function($scope, $http) {
   $http({method: 'GET', url: '/v1/posts'}).
     success(function(data, status, headers, config) {
-      $scope.posts = [data.items[0]];
+      $scope.posts = data.items;
+      $scope.image = data.items[0].post_images[0];
+      console.log($scope.image);
     });
 });
