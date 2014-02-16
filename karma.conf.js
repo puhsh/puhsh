1bf4d74086ca5bc92d6f8af966b1e852eb88297f
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-16 10:32:24 -0600

    Growl for Karma

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress', 'dots'],
+    reporters: ['progress', 'dots', 'growl'],
 
 
     // web server port
