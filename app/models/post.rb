c371bdffce6132dd81d33a0c51570c6367b0f0c5
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-17 15:47:58 -0600

    Enable scopes for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,12 +6,12 @@ class Post < ActiveRecord::Base
 
   attr_accessible :title, :description, :pick_up_location, :payment_type, :category, :category_id, :subcategory, :subcategory_id, :city, :user_id, :item_attributes, :user, :post_images_attributes, :status
   symbolize :pick_up_location, in: { porch: 'Porch Pick Up', public_location: 'Meet at Public Location', house: 'Pickup at House', other: 'Other' },
-            methods: true, scope: false, i18n: false, validate: false
+            methods: true, scopes: false, i18n: false, validate: false
 
   symbolize :payment_type, in: { both: 'Insta-Payment & Cash', cash: 'Cash Only', insta_payment: 'Insta-Payment Only', free: 'Item is Free' },
-            methods: true, scope: false, i18n: false, validate: false
+            methods: true, scopes: false, i18n: false, validate: false
 
-  symbolize :status, in: [:for_sale, :offer_accepted, :withdrawn_by_seller, :sold], methods: true, scope: false, validate: false, default: :for_sale
+  symbolize :status, in: [:for_sale, :offer_accepted, :withdrawn_by_seller, :sold], methods: true, scopes: :shallow, validate: false, default: :for_sale
 
   # Relations
   has_one :item, dependent: :destroy
