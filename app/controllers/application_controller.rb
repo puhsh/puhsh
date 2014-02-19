e42deaf7cfdab7fd6ba28200a00b414ae3d7e815
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-19 13:19:58 -0600

    Re-enable sandbox

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :check_if_production
 
   def peek_enabled?
     current_user_admin?
@@ -8,11 +7,5 @@ class ApplicationController < ActionController::Base
 
   def current_user_admin?
     current_user.admin? if current_user
-  end
-
-  protected 
-
-  def check_if_production
-    redirect_to 'http://www.puhsh.com' if Rails.env.sandbox?
   end
 end
