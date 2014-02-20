de24ffff585ef999d5fb43b17f74e137913f17ab
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-20 16:23:16 -0600

    [#93] Provide additional avatar urls the client can use.

--- app/serializers/simple_user_serializer.rb
+++ app/serializers/simple_user_serializer.rb
@@ -1,7 +1,7 @@
 class SimpleUserSerializer < ActiveModel::Serializer
   attributes :id, :uid, :name, :first_name, :last_name, :avatar_url, :zipcode, :location_description, :latitude,
              :longitude, :gender, :facebook_email, :contact_email, :posts_count, :posts_flagged_count,
-             :star_count, :is_following
+             :star_count, :is_following, :other_avatar_urls
 
   def is_following
     current_user ? current_user.following?(object) : false
--- lib/puhsh/facebook.rb
+++ lib/puhsh/facebook.rb
@@ -1,17 +1,28 @@
 module Puhsh
   module Facebook
-    # Returns a Facebook user Hash if the given Facebook user ID and access token
-    # identifiy a Verified Facebook user, nil otherwise.
     def self.find_verified_user(fb_id, fb_access_token)
       fb = Koala::Facebook::API.new(fb_access_token)
       fb_record = fb.get_object('me')
       fb_record['id'] == fb_id && verified?(fb_record) ? fb_record : nil
     end
 
+    def facebook_avatar_url_with_size(original_url, size)
+      if size
+        base_url = facebook_base_avatar_url(original_url)
+        base_url + "?type=#{size.to_s}"
+      else
+        original_url
+      end
+    end
+
     private
 
     def self.verified?(fb_record)
       fb_record['verified'] || FacebookTestUser.find_by_fbid(fb_record['id']).present?
+    end
+
+    def facebook_base_avatar_url(url)
+      url.split('?').first
     end
   end
 end
