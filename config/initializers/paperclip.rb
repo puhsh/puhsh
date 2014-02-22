f42ba0af839c56aeb25ab30d9347234b573361e3
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-22 10:12:16 -0600

    Revert "Let there be CDN"
    This reverts commit 78642a3b99dfc95aa47941b0c67fa04d3fb84510.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,7 +1,6 @@
 s3_credentials = YAML.load_file("#{Rails.root}/config/s3.yml")[Rails.env].symbolize_keys
 Paperclip::Attachment.default_options[:storage] = :s3
-Paperclip::Attachment.default_options[:url] = s3_credentials[:url]
-Paperclip::Attachment.default_options[:s3_host_alias] = s3_credentials[:host_alias]
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:s3_credentials] = s3_credentials
 Paperclip::Attachment.default_options[:s3_protocol] = 'http'
 Paperclip::Attachment.default_options[:s3_permissions] = :private
