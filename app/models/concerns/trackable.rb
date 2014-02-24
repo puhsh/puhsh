b5eefa7260a02b4c82e2f0f6b813b55aa7376ae1
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-24 15:11:46 -0600

    Track genders for users too.

--- app/models/concerns/trackable.rb
+++ app/models/concerns/trackable.rb
@@ -8,7 +8,12 @@ module Trackable
   protected
 
   def increment_create
-    if Rails.env.production?
+    return unless Rails.env.production?
+    case self
+    when User
+      Puhsh::StatsdReporting.send_increment_to_statsd(self.class, 'create')
+      Puhsh::StatsdReporting.send_increment_to_statsd(self.class, "create_#{self.gender}")
+    else
       Puhsh::StatsdReporting.send_increment_to_statsd(self.class, 'create')
     end
   end
