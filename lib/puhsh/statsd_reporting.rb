814e76ed980e0989281d65220cc2858ed2175e71
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-24 15:07:46 -0600

    Another key update

--- lib/puhsh/statsd_reporting.rb
+++ lib/puhsh/statsd_reporting.rb
@@ -12,7 +12,7 @@ module Puhsh
     end
 
     def self.send_increment_to_statsd(name, action)
-      key = "production.#{name}.#{action}"
+      key = "production.totals.#{name}.#{action}"
       $statsd.increment key
     end
   end
