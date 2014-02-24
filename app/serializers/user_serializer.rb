c5915656378f110a2c21e0f17699cc2d96d8b0ad
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-02-24 12:34:18 -0600

    Exposer other avatar urls

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,12 +1,16 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :uid, :name, :first_name, :last_name, :avatar_url, :zipcode, :location_description, :latitude,
              :longitude, :home_city, :gender, :facebook_email, :contact_email, :posts_count, :posts_flagged_count,
-             :app_invite, :star_count, :is_following, :other_avatar_urls
+             :app_invite, :star_count, :is_following, :avatar_urls
 
   has_one :home_city
   has_one :app_invite
 
   def is_following
     current_user ? current_user.following?(object) : false
+  end
+
+  def avatar_urls
+    object.other_avatar_urls
   end
 end
