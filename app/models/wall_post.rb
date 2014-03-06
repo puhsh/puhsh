89bfdf4e442773b0c019561889863c5fd2028037
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-03-05 20:34:13 -0600

    Can only do a SMS share one time

--- app/models/wall_post.rb
+++ app/models/wall_post.rb
@@ -19,8 +19,11 @@ class WallPost < ActiveRecord::Base
   protected
 
   def limit_type_of_wall_posts
-    if WallPost.where(user_id: self.user_id, post_type: self.post_type).count > 3
+    if WallPost.where(user_id: self.user_id, post_type: :alpha_share).count > 3
       errors.add(:post_type, 'cannot have more than three wall posts of this type')
+    end
+    if WallPost.where(user_id: self.user_id, post_type: :sms_share).count > 1
+      errors.add(:post_type, 'cannot have more than one wall post of this type')
     end
   end
 
