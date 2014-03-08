d328a5f36c58ae47588d4e3e69ae00ca94f097ed
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-03-08 11:31:58 -0600

    Another typo...jeez.

--- app/models/related_product.rb
+++ app/models/related_product.rb
@@ -40,7 +40,7 @@ class RelatedProduct
 
     if response_hash && response_hash['ItemSearchResponse']
       items = response_hash['ItemSearchResponse']['Items']
-      item = items['Items'].try(&:first)
+      item = items['Item'].try(&:first)
     end
 
     if item.present? && item['ItemAttributes'].present?
