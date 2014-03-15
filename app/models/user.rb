23095cc4306119b0128459c044f97ec5662799d0
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-03-15 09:42:23 -0500

    Basic city route working.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   include Redis::Objects
   include Trackable
   include OpenGraphed
-  include FriendlyId
 
   INVITES_ENABLED = Rails.env.development? ? false : true
   ALPHA_ENABLED = Rails.env.development? ? false : true
@@ -13,7 +12,6 @@ class User < ActiveRecord::Base
   devise :trackable, :omniauthable, omniauth_providers: [:facebook]
   rolify
   geocoded_by :zipcode
-  friendly_id :full_name
 
   # Relations
   has_many :posts
@@ -165,10 +163,6 @@ class User < ActiveRecord::Base
 
   def change_unsold_posts_city!
     Post.for_sale.where(user_id: self.id).update_all(city_id: self.city_id)
-  end
-
-  def full_name
-    "#{self.first_name}-#{self.last_name}"
   end
 
   def home_city_changed?
