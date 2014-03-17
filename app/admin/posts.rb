58ebb4fc4110bc47cefabfbc791b2688b54bec36
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-03-17 07:31:53 -0500

    Admin stuff

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -5,5 +5,19 @@ ActiveAdmin.register Post do
 
   filter :title
   filter :description
-  
+
+  index do
+    column :id
+    column :title
+    column :slug
+    column :description
+    column :pick_up_location
+    column :payment_type
+    column :status
+    column :created_at
+    column :updated_at
+    actions do |post|
+      link_to 'View Page', post_path(post.city_id, post.user_id, post.id), target: :blank
+    end
+  end
 end
