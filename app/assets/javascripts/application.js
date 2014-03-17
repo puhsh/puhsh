6dec0c9ac797c8d54747fa1bb7ef293916967450
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-03-16 22:38:24 -0500

    Weird stuff going on with the asset pipeline.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,11 +2,6 @@
 //= require foundation
 //= require peek.js
 //= require peek/views/performance_bar
-//= require moment/moment.js
-//= require sly/src/sly.js
-//= require spin.js/spin.js
-//= require mousetrap/mousetrap.js
-//= require underscore/underscore.js
 
 
 $(function(){ $(document).foundation(); });
