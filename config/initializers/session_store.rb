a2d6e383ffa8156c9b9c3288946127548d9f710d
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-03-20 14:31:27 -0500

    Switching session store.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-Puhsh::Application.config.session_store :cookie_store, key: '_puhsh_session'
+# Puhsh::Application.config.session_store :cookie_store, key: '_puhsh_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Puhsh::Application.config.session_store :active_record_store
+config =  YAML.load_file("#{Rails.root}/config/redis.yml")[Rails.env]
+Puhsh::Application.config.session_store :redis_store, config.symbolize_keys!
