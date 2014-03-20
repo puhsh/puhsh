1c5bbcab537a2fe87022db4426848dc8549c2063
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-03-19 22:43:05 -0500

    Another image size

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,7 +3,7 @@ class PostImage < ActiveRecord::Base
   
   # Paperclip Attributes
   has_attached_file :image,
-                    styles: { original: '1280x1280#', medium: '640x640#' },
+                    styles: { original: '1280x1280#', medium: '640x640#', small: '320x320#' },
                     s3_permissions: :public_read,
                     path: "posts/:post_id/post_images/:slug-:id-:style.:extension"
 
@@ -19,6 +19,7 @@ class PostImage < ActiveRecord::Base
 
   def image_urls
     {
+      small: self.image.url(:small),
       medium: self.image.url(:medium),
       large: self.image.url(:large)
     }
