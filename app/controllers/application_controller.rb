c134dd3e1966750877059a8d4b601f6ba3cb3425
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-03-22 10:11:22 -0500

    More admin fixes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,8 @@ class ApplicationController < ActionController::Base
 
   respond_to :html
 
-  rescue_from ActiveRecord::RecordNotFound, with: :not_found!
-  rescue_from ActionController::RoutingError do |exception|
-    not_found!
-  end
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActionController::RoutingError, with: :not_found
 
   def peek_enabled?
     current_user_admin?
@@ -22,7 +20,7 @@ class ApplicationController < ActionController::Base
 
   protected 
 
-  def not_found!
+  def not_found
     redirect_to 'http://www.puhsh.com/404' unless Rails.env.development?
   end
 end
