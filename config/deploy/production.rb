4ad3f9a8fe261c7d43758a76fec7527cd343cc8f
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-03-31 10:07:40 -0500

    New app server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
-server '75.126.213.98', user: 'puhsh', roles: %w{web app solr db}
+server '75.126.213.98', user: 'puhsh', roles: %w{web app job db}
+server '50.23.243.61', user: 'puhsh', roles: %w{web app}
 
 set :application, 'puhsh'
 set :deploy_to, "/web/puhsh"
--- config/schedule.rb
+++ config/schedule.rb
@@ -11,7 +11,7 @@
 ##################
 
 if environment == 'production'
-  every :day, at: '1:30 am', roles: [:app]  do
+  every :day, at: '1:30 am', roles: [:job]  do
     rake 'db:backup', environment: 'production'
   end
 end
