9b2d5239130669940804ccdcf54af0fc5d118736
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-04-04 14:39:42 -0500

    event updates

--- config/initializers/instrumentation.rb
+++ config/initializers/instrumentation.rb
@@ -32,8 +32,8 @@ end
 
 
 ActiveSupport::Notifications.subscribe /event/ do |name, start, finish, id, payload|
-  payload = payload[:value]
-  if Rails.env.production? && payload.present?
+  payload = HashWithIndifferentAccess.new(payload[:value])
+  if Rails.env.production? && payload.present? && !payload[:user_id].nil?
     opts = { 
       user_id: payload[:user_id], user_ip_address: payload[:user_ip_address], 
       resource_id: payload[:params][:id], resource_type: payload[:resource_type], 
--- lib/puhsh/jobs/event_job.rb
+++ lib/puhsh/jobs/event_job.rb
@@ -14,7 +14,7 @@ module Puhsh
       def record_event(opts)
         opts = HashWithIndifferentAccess.new(opts)
         if valid_event?(opts) && PUHSH_EVENTS[:tracking_enabled]
-          create_event!(opts)
+          self.create_event!(opts)
         end
       end
 
@@ -26,7 +26,7 @@ module Puhsh
           f.adapter  Faraday.default_adapter
         end
 
-        opts = { userId: opts[:user_id], userIPAddress: opts[:user_ip_address], resourceId: opts[:resource_id], resourceType: opts[:resource_type], controllerName: opts[:controller_name], controllerAction: opts[:controller_action] }
+        opts = { userId: opts[:user_id], userIPAddress: opts[:user_ip_address], resourceId: opts[:resource_id].to_i, resourceType: opts[:resource_type], controllerName: opts[:controller_name], controllerAction: opts[:controller_action] }
 
         conn.post do |req|
           req.url '/events'
