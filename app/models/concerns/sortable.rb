82e72254a5d858944459e4a2977f9d8d0f613fce
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-04-08 17:19:41 -0500

    Rails 4 requires procs for scopes. Fix deprecation warnings.

--- app/models/concerns/sortable.rb
+++ app/models/concerns/sortable.rb
@@ -2,11 +2,11 @@ module Sortable
   extend ActiveSupport::Concern
 
   included do
-    scope :recent, order('created_at desc')
-    scope :oldest, order('created_at asc')
-    scope :latest, order('updated_at desc')
-    scope :newest, order('id desc')
-    scope :alpha, order('name asc')
-    scope :reverse_alpha, order('name desc')
+    scope :recent, -> { order('created_at desc') }
+    scope :oldest, -> { order('created_at asc') }
+    scope :latest, -> { order('updated_at desc') }
+    scope :newest, -> { order('id desc') }
+    scope :alpha, -> { order('name asc') }
+    scope :reverse_alpha, -> { order('name desc') }
   end
 end
