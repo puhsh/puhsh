cb5d1bdc975081e51211317c185ee63e7b932798
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-04-09 07:21:33 -0500

    Friendly ID Fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,10 @@ class PostsController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:user_id])
-    @city = City.find(params[:city_id])
+    @user = User.friendly.find(params[:user_id])
+    @city = City.friendly.find(params[:city_id])
     if @user && @city
-      @post = @user.posts.includes(:item, :post_images, {user: :home_city}).find(params[:id], conditions: ['city_id = ?', @city.id])
+      @post = @user.posts.includes(:item, :post_images, {user: :home_city}).friendly.where('posts.city_id = ?', @city.id).find(params[:id])
       @image_count = @post.post_images.count if @post
     end
     respond_with @post
