ab09973bb5475fa4afbc27e5bda364c999c9a4d7
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-04-16 16:21:05 -0500

    Updated the Sortable concern to properly reference the tables it was using.

--- app/models/concerns/sortable.rb
+++ app/models/concerns/sortable.rb
@@ -2,11 +2,11 @@ module Sortable
   extend ActiveSupport::Concern
 
   included do
-    scope :recent, -> { order('created_at desc') }
-    scope :oldest, -> { order('created_at asc') }
-    scope :latest, -> { order('updated_at desc') }
-    scope :newest, -> { order('id desc') }
-    scope :alpha, -> { order('name asc') }
-    scope :reverse_alpha, -> { order('name desc') }
+    scope :recent, -> { order("#{table_name}.created_at desc") }
+    scope :oldest, -> { order("#{table_name}.created_at asc") }
+    scope :latest, -> { order("#{table_name}.updated_at desc") }
+    scope :newest, -> { order("#{table_name}.id desc") }
+    scope :alpha, -> { order("#{table_name}.name asc") }
+    scope :reverse_alpha, -> { order("#{table_name}.name desc") }
   end
 end
