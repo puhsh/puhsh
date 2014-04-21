7e383a4898c2cd67afce51e93e5b1d20604624f8
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-04-21 17:54:55 -0500

    Sitemap config. Ready to generate when we are ready.

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -3,4 +3,6 @@ require 'sitemap_generator'
 
 SitemapGenerator::Sitemap.default_host = 'http://puhsh.com'
 SitemapGenerator::Sitemap.create do
+  add '/download', changefreq: 'monthly'
+  add '/posts', changefreq: 'weekly'
 end
