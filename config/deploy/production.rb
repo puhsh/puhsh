51e941cdc2545c3ac9a0277b25638defc70c608f
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-04-21 10:54:28 -0500

    Cron fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-server '75.126.213.98', user: 'puhsh', roles: %w{web app job db}
+server '75.126.213.98', user: 'puhsh', roles: %w{web app db}
 server '50.23.243.61', user: 'puhsh', roles: %w{web app}
-server '75.126.213.100', user: 'puhsh', roles: %w{resque}
+server '75.126.213.100', user: 'puhsh', roles: %w{resque job}
 
 set :application, 'puhsh'
 set :deploy_to, "/web/puhsh"
