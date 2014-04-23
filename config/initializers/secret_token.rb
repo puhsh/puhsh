4bfd87d037cf1ee7279c487b032f53abfb21c4d0
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-04-23 12:34:31 -0500

    Typo

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,4 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Puhsh::Application.config.secret_token = '4ee545518c182962db256546fce7c526d18d43eea7c5d8473c2add119702a8741832d619a437c6fb7ec29dcd13defbfeacc0e72245d19396313c677aa258b858'
-Puhsh::Application.config.secret_base = 'a84cf5a928e71212e0673b4e208ce414735a7a47d6610ba4fa6cd399f9d691125df774699a9abe6c8141432707aff30c3491b0dc5f4ae505479675bd5898a2e5'
+Puhsh::Application.config.secret_key_base = 'a84cf5a928e71212e0673b4e208ce414735a7a47d6610ba4fa6cd399f9d691125df774699a9abe6c8141432707aff30c3491b0dc5f4ae505479675bd5898a2e5'
