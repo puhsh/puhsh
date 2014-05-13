97ed60d30c1a2357650e4d890999aa92ff74f6a4
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-05-12 20:52:47 -0500

    N+1 issue

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.includes({post_images: :post}).recent
+    @posts = Post.includes({post_images: :post}, :item).recent
     respond_with @posts
   end
 
