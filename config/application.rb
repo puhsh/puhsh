622cca4888b14db3b930a5aa155294e4dc3502ac
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-05-20 13:40:43 -0500

    Bump asset version.

--- config/application.rb
+++ config/application.rb
@@ -62,7 +62,7 @@ module Puhsh
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
 
     # Include Bower Assets
     config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
