640e40c8c7f9de240b49e6880e80b29f325e055e
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-05-20 15:56:03 -0500

    No clean before.

--- config/initializers/bower_rails.rb
+++ config/initializers/bower_rails.rb
@@ -1,4 +1,4 @@
 BowerRails.configure do |bower_rails|
   bower_rails.install_before_precompile = true # invokes rake bower:install before precompilation
-  bower_rails.clean_before_precompile = true   # invokes rake bower:clean before precompilation
+  # bower_rails.clean_before_precompile = true   # invokes rake bower:clean before precompilation
 end
