85ad0ee84aee053ee4ce8e61f7d34bb5cd7553fe
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-05-20 15:40:21 -0500

    Bad Redirect error

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -6,12 +6,6 @@ class CitiesController < ApplicationController
       @posts = @city.posts.includes(:item, :user, :city, {post_images: :post}).page(params[:page]).per(10).recent
     end
 
-    respond_with @posts do |format|
-      if @posts
-        format.html
-      else
-        redirect_to root_url
-      end
-    end
+    respond_with @posts
   end
 end
