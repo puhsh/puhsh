28eaede441bc3ab0964d4e1cb02a48d0516f64e8
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-05-21 10:39:59 -0500

    Sorting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.includes(:home_city, posts: [:city, :item, {post_images: :post}]).friendly.find(params[:user_id])
     @city = @user.home_city
-    @posts = @user.posts.page(params[:page]).per(10)
+    @posts = @user.posts.page(params[:page]).per(10).recent
     respond_with @posts
   end
 end
