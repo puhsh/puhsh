2cbf2bd1cd01e7ceadef3fd03a1abaca9e1da54f
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-05-26 11:02:46 -0500

    User founding section.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   # TODO this is a n+1 mess
   def show
-    @user = User.includes(:home_city, posts: [:city, :item, {post_images: :post}]).friendly.find(params[:user_id])
+    id = params[:user_id] || params[:id]
+    @user = User.includes(:home_city, posts: [:city, :item, {post_images: :post}]).friendly.find(id)
     @city = @user.home_city
     @posts = @user.posts.page(params[:page]).per(10).recent
     respond_with @posts
