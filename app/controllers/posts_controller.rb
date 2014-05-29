a87274ab2e45ec9bd659c18cdbd9231742e1ad0c
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-05-29 15:08:12 -0500

    Paving the way for a new post page.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_filter :hide_header, only: [:show]
+
   def index
     @posts = Post.includes({post_images: :post}, :item, :city, :user).page(params[:page]).per(20).recent
     respond_with @posts
