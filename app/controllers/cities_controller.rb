aa1852c6ace6ff44bc43494888c120e5ffb79176
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-06-22 09:50:59 -0500

    Founding user bug

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -8,7 +8,7 @@ class CitiesController < ApplicationController
 
     if @city
       @posts = @city.posts.includes(:item, :user, :city, {post_images: :post}).page(params[:page]).per(20).recent
-      @founding_user = @posts.to_a.first.try(&:user)
+      @founding_user = @city.home_users.where('users.posts_count > ?', 1).oldest.first
     end
 
     respond_with @posts do |format|
