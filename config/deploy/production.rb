0417e579062423ec2362acc3ab32e425adfb5435
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-06-27 22:14:58 -0500

    App2 is no more

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 server '75.126.213.98', user: 'puhsh', roles: %w{web app db}
-server '50.23.243.61', user: 'puhsh', roles: %w{web app}
+# server '50.23.243.61', user: 'puhsh', roles: %w{web app}
 server '75.126.213.100', user: 'puhsh', roles: %w{resque job}
 
 set :application, 'puhsh'
