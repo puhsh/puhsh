72b44ac4595c05ccab336aa6a2c1a42dc39e914b
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-09-17 15:39:42 -0500

    Server cleanup

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,4 @@
-server '75.126.213.98', user: 'puhsh', roles: %w{web app db}
-# server '50.23.243.61', user: 'puhsh', roles: %w{web app}
-server '75.126.213.100', user: 'puhsh', roles: %w{resque job}
+server '75.126.213.98', user: 'puhsh', roles: %w{web app db resque job}
 
 set :application, 'puhsh'
 set :deploy_to, "/web/puhsh"
--- config/deploy/sandbox.rb
+++
@@ -1,7 +0,0 @@
-server '50.23.243.59', user: 'puhsh', roles: %w{web app db, job}
-
-set :application, 'sandbox.puhsh'
-set :deploy_to, "/web/sandbox.puhsh"
-set :stage, :sandbox
-set :rails_env, 'sandbox'
-set :branch, ENV['BRANCH'] || 'master'
