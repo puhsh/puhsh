1be42141996e918a9976d5a2360d00a9800c252d
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-09-18 21:06:51 -0500

    Revert "I need better logging"
    This reverts commit c24ca259e8d2e1056e1b4c0d09fd980b953201ef.

--- app/models/zipcode.rb
+++ app/models/zipcode.rb
@@ -1,5 +1,5 @@
 class Zipcode < ActiveRecord::Base
-  attr_accessible :city, :state, :latitude, :longitude, :code, :city_id, :city_name
+  attr_accessible :city, :state, :latitude, :longitude, :code, :city_id
   geocoded_by :address
 
   # Relations
