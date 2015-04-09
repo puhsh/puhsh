f551613f3db2f45867b94552fe79663ff113613a
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2015-04-09 07:44:24 -0500

    Fix images on the web

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,7 +1,7 @@
 s3_credentials = YAML.load_file("#{Rails.root}/config/s3.yml")[Rails.env].symbolize_keys
 Paperclip::Attachment.default_options[:storage] = :s3
 Paperclip::Attachment.default_options[:s3_credentials] = s3_credentials
-Paperclip::Attachment.default_options[:s3_protocol] = 'https'
+Paperclip::Attachment.default_options[:s3_protocol] = 'http'
 Paperclip::Attachment.default_options[:s3_permissions] = :private
 
 Paperclip.interpolates :post_id do |attachment, style|
